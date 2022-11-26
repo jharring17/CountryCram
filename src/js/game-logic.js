d54fb846b18ce28219bf7f8d5bac56c871e78fdf
hli1997 <lihaodong97@gmail.com>
2022-11-26 01:06:54 -0500

    beta version on Europe page, still hae work to do

---
+++ src/js/game-logic.js
@@ -0,0 +1,95 @@
+let flagImgDiv = document.querySelector('.flag-img');
+let flagImg = document.querySelector('.flag-img img');
+let flagOptions = document.querySelector('.button-container');
+let flagArray = document.querySelectorAll('.button-container button');
+
+let resultDiv = document.querySelector('.result');
+let score = document.querySelector('.result .score span');
+
+// let continent = document.querySelector('.body')
+
+let index = 0;
+let numOfCorrect= 0;
+
+function newGame() {
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) { 
+            let questions = JSON.parse(this.responseText);
+
+            //ToDo: add fuction to select num of questions
+            let questionNum = 5;
+            // Get a random index.
+            questions = questions.sort(() => Math.random() - Math.random()).slice(0, questionNum);
+            //Add Questions Data
+            addData(questions[index], questionNum);
+
+            flagArray.forEach(button => {
+                button.addEventListener('click', () => {
+                    let answer = questions[index].answer;
+                    button.classList.add('selected');
+                    //Increase Index 
+                    index++;
+                    //Check The Answer
+                    setTimeout(() => {
+                        check(answer);
+                    }, 500);
+
+                    setTimeout(() => {
+                        flagImg.src = '';
+                        button.classList.remove('selected');
+                        button.classList.remove('right');
+                        button.classList.remove('wrong');
+                        addData(questions[index], questionNum);
+                    }, 1000);
+
+                    setTimeout(() => {
+                        getResult(questionNum);
+                    }, 1002);
+                });
+            });
+        }
+    }
+    // if(continent == 'europe'){
+    //     myRequest.open("GET", "src/json/europe.json", true);
+    // }
+    request.open("GET", "/src/data/europe.json", true);
+    request.send();
+}
+
+function addData(obj, count) {
+    if (index < count) {
+        flagImg.src = `/src/images/countries/Europe/${obj.img}`;
+        flagArray.forEach((button, i) => {
+            button.id = `answer_${i+1}`;
+            button.dataset.options = obj[`options`][i];
+            button.innerHTML = obj[`options`][i];
+        });
+    }
+}
+
+function check(answer) {
+    for (let i = 0; i < flagArray.length; i++) {
+        if (flagArray[i].classList.contains('selected')) {
+            let selectedOption = flagArray[i].dataset.options;
+            if (selectedOption ===  answer) {
+                flagArray[i].classList.add('right');
+                numOfCorrect++;
+            } else {
+                flagArray[i].classList.add('wrong');
+            }
+        }
+    }
+}
+
+function getResult(count) {
+    if (index === count) {
+        flagOptions.style.display = 'none'
+        flagImgDiv.style.display = 'none'
+        resultDiv.style.display = 'grid'
+        score.innerHTML = numOfCorrect;
+    }
+}
+
+
+newGame();
